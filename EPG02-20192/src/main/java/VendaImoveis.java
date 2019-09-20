aeac780d5da95708f33a283f73ff2d8ff44ac1dc
machado.patricia <patricia@computacao.ufcg.edu.br>
2019-09-20 12:35:59 -0300

    Criação

---
+++ EPG02-20192/src/main/java/VendaImoveis.java
@@ -0,0 +1,28 @@
+
+import java.util.Set;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+public class VendaImoveis {
+	
+	Graph <String,DefaultWeightedEdge> distrito;
+	
+	VendaImoveis (String fileName) {
+		/* Adicione aqui o código que cria uma instancia da classe grafo desejada a partir de um arquivo
+		 * cujo nome está definido em filename.
+		 * Veja métodos para importação de arquivos em MyJGraphTUtil.java
+		 */
+	}
+	
+	public String localizaImovel (String pontodeInteresse, Set <String> imoveis) {
+		/*
+		 * Adicione aqui o código que recebe um ponto de interesse como entrada, uma lista de imóveis e retorna o imóvel disponível mais próximo a este ponto de interesse.
+		 * Note que tanto o ponto de interesse quanto o imóvel a ser selecionado são vértices do grafo.
+		 */
+		return null;
+
+	}
+	
+	
+
+}
